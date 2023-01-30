606bd41dc78f6894b7ae033d412efe60b1df8f25
Weronika <weronika.lopata@gmail.com>
2023-01-30 14:52:31 +0100

    starter project

---
+++ backend/day-by-day-planner/src/main/java/com/ikadev/daybydayplanner/DayByDayPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.ikadev.daybydayplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayByDayPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayByDayPlannerApplication.class, args);
+	}
+
+}
---
+++ backend/day-by-day-planner/src/test/java/com/ikadev/daybydayplanner/DayByDayPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikadev.daybydayplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayByDayPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
